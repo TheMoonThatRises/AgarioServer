ef34c4eb260887ff2a44b6b12a65a8de87da87ba
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-04-22 14:22:36 -0600

    decrease physics engine map size

--- src/ceccs/game/utils/PhysicsMap.java
+++ src/ceccs/game/utils/PhysicsMap.java
@@ -2,7 +2,7 @@ package ceccs.game.utils;
 
 public class PhysicsMap {
 
-    final public static double width = 5_000;
-    final public static double height = 5_000;
+    final public static double width = 500;
+    final public static double height = 500;
 
 }
