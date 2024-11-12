fcd960b700a57bffc924db296f24ead8316d702f
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-11-12 13:45:36 -0700

    fix map params
    need to fix redundant player calculations in chunks causing improper physics and position calculations

--- src/ceccs/game/utils/PhysicsMap.java
+++ src/ceccs/game/utils/PhysicsMap.java
@@ -2,10 +2,10 @@ package ceccs.game.utils;
 
 public class PhysicsMap {
 
-    final public static double width = 5000;
-    final public static double height = 5000;
+    final public static double width = 700;
+    final public static double height = 700;
 
-    final public static int chunkWidth = 15;
-    final public static int chunkHeight = 15;
+    final public static int chunkWidth = 25;
+    final public static int chunkHeight = 25;
 
 }
